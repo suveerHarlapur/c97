d9a6bec3a4cf8a528ffe2178ecbd8843480e8ee5
suveerHarlapur <85638708+suveerHarlapur@users.noreply.github.com>
2021-07-14 13:45:59 +0530

    Update chances.py

--- chances.py
+++ chances.py
@@ -1,46 +1,20 @@
-chances = 5
-guessnumber = input('type a number between 1 and 9  ')
-print('guessed number  ',guessnumber)
-if(chances>=0):
-    if(guessnumber=='2'):
-        print('the guess is incorrect ,it is not so close to correct guess')
-        print('chances remaining')
-        chances=chances-1
-        print(chances)
+print("...Number Guessing Game...")
+print("guess a number (beetween 1 and 9) :")
+import random
+number=random.randint(2,8)
+guess=int(input("Enter A Number :"))
+chances=5
 
-    if(guessnumber=='3'):
-        print('the guess is incorrect ,it is a little to correct guess')
-        print('chances remaining')
-        chances=chances-1
-        print(chances)
-    if(guessnumber=='4'):
-        print('the guess is incorrect ,it is little close to correct guess')
-        print('chances remaining')
-        chances=chances-1 
-        print(chances)
-    if(guessnumber=='5'):
-        print('the guess is incorrect ,it is  close to correct guess')
-        print('chances remaining')
-        chances=chances-1
-        print(chances)
-    if(guessnumber=='6'):
-        print('the quess is correct you won the game')
-        print('yoy still have ')
-        print(chances)
-    if(guessnumber=='7'):
-        print('the guess is incorrect ,it is a little far to correct guess')
-        print('chances remaining')
-        chances=chances-1
-        print(chances)
-    if(guessnumber=='8'):
-        print('the guess is incorrect ,it is little far to correct guess')
-        print('chances remaining')
-        chances=chances-1
-        print(chances)
-    if(guessnumber=='9'):
-        print('the guess is incorrect ,it is not so close to correct guess')
-        print('chances remaining')
-        chances=chances-1
-        print(chances)
-elif(chances=='0'):
-    print('chances over soo game is over')
+while chances>0:
+    if(guess==number):
+        print("you won !!!")
+        break
+    else:
+        chances-=1
+        if(chances>0):
+            print('the guessed number is incorrect it is not close to ',guess)
+            print("remaining chances ",chances)
+            guess=int(input("Enter A Number :"))
+        elif(chances==0):
+            print("game over the correct answer is",number)
+            break
